0717fc281087f88f4cae87e88b5fb70f3a0ae75b
JorgeLuisMartinez <Jorge389.jm@gmail.com>
2023-09-28 11:06:09 -0500

    Clases 1-7 inyeccion de dependencias, useValue useClass y useFactory, hacer peticiones haciendo uso de httpModules httpServices

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,30 @@
-import { Module } from '@nestjs/common';
+import { HttpModule, Module, HttpService } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 
+const API_KEY= '123456789';
+const PROD_API_KEY= 'AAAAA123456789';
+
 @Module({
-  imports: [UsersModule, ProductsModule],
+  imports: [HttpModule ,UsersModule, ProductsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: 'API_KEY',
+      useValue: process.env.NODE_ENV === 'production' ? PROD_API_KEY : API_KEY,
+    },
+    {
+      provide: 'TASKS',
+      useFactory: async (http: HttpService) =>{
+        // Como realizar la peticion a la API
+        const tasks  = await http.get('https://jsonplaceholder.typicode.com/todos').toPromise();
+        return tasks.data;
+      },
+      inject: [HttpService]
+    },
+  ],
 })
 export class AppModule {}
