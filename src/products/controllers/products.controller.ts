38a9301edd9f83c5de45698856e7e2b0098bf92f
JorgeLuisMartinez <Jorge389.jm@gmail.com>
2023-10-02 12:56:45 -0500

    Crud productos complete

--- src/products/controllers/products.controller.ts
+++ src/products/controllers/products.controller.ts
@@ -38,21 +38,21 @@ export class ProductsController {
   @Get(':productId')
   @HttpCode(HttpStatus.ACCEPTED)
   getOne(@Param('productId', ParseIntPipe) productId: number) {
-    return this.productsService.findOneBy(productId);
+    return this.productsService.findOne(productId);
   }
 
-  // @Post()
-  // create(@Body() payload: CreateProductDto) {
-  //   return this.productsService.create(payload);
-  // }
+  @Post()
+  create(@Body() payload: CreateProductDto) {
+    return this.productsService.create(payload);
+  }
 
-  // @Put(':id')
-  // update(@Param('id') id: string, @Body() payload: UpdateProductDto) {
-  //   return this.productsService.update(+id, payload);
-  // }
+  @Put(':id')
+  update(@Param('id') id: number, @Body() payload: UpdateProductDto) {
+    return this.productsService.update(id, payload);
+  }
 
-  // @Delete(':id')
-  // delete(@Param('id') id: string) {
-  //   return this.productsService.remove(+id);
-  // }
+  @Delete(':id')
+  delete(@Param('id') id: number) {
+    return this.productsService.remove(+id);
+  }
 }
