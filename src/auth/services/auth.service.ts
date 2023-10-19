998dd3f01cb94143c86bc2b0253950bc5c6001ae
JorgeLuisMartinez <Jorge389.jm@gmail.com>
2023-10-19 11:46:11 -0500

    clase 10 estable

---
+++ src/auth/services/auth.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+
+import { UsersService } from './../../users/services/users.service';
+import * as bcrypt from 'bcrypt'
+
+@Injectable()
+export class AuthService {
+  constructor(private userService: UsersService){}
+
+  async validateUser(email:string, password:string){
+    const user = await this.userService.findByEmail(email);
+    if (user) {
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (isMatch) {
+        return user;
+      }
+    }
+    return null
+  }
+
+}
